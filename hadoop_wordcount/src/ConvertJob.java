381dab52df04458813af7f6e1ae54e95dcd98802
Brad Liu <brad_liu@trend.com.tw>
2013-08-29 17:19:50 +0800

    finish reducer part

--- hadoop_wordcount/src/ConvertJob.java
+++ hadoop_wordcount/src/ConvertJob.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,11 +17,12 @@ import org.apache.hadoop.util.*;
 
 public class ConvertJob extends Configured implements Tool {
     private static final Log LOG = LogFactory.getLog(ConvertJob.class);
+    private static Pattern datePatt = Pattern.compile("(.*)=(.*)");
     
     public static class Map extends Mapper<LongWritable, Text, Text, Text> {
         private Set<String> fields = new HashSet<String>(); // necessary fields set
         private java.util.Map<String, String> nameMap = new HashMap<String, String>(); // field name convert map
-        private static Pattern datePatt = Pattern.compile("(.*)=(.*)");
+        
         
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
@@ -92,7 +95,7 @@ public class ConvertJob extends Configured implements Tool {
     
     public static class Reduce extends Reducer<Text, Text, Text, Text> {
         private static Pattern eventTimePatt = Pattern.compile(".*event_time_start=(\\d+),.*");
-        
+        private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         /*
          * sort by event_time_start
          */
@@ -105,14 +108,14 @@ public class ConvertJob extends Configured implements Tool {
                     return -1;
                 }
                 
-                LOG.info("o1 value=" + m1.group(1) + "," + "o2 value=" + m2.group(1));
+                //LOG.info("o1 value=" + m1.group(1) + "," + "o2 value=" + m2.group(1));
                 
                 return o1.compareTo(o2);
             }
         }
         
-        private List<String> sortByEvent_create_dt(Iterable<Text> values) {
-            List<String> sortedList = new LinkedList<String>();
+        private LinkedList<String> sortByEvent_create_dt(Iterable<Text> values) {
+            LinkedList<String> sortedList = new LinkedList<String>();
             for (Text value:values) {
                 sortedList.add(value.toString());
             }
@@ -120,18 +123,103 @@ public class ConvertJob extends Configured implements Tool {
             Collections.sort(sortedList, new EventTimeComparator());
             return sortedList;
         }
+             
+        private HashMap<String, String> convertToMap(String string) {             
+            java.util.HashMap<String, String> map = new HashMap<String, String>();
+            String[] nameValueArray = string.toString().split(",");
+            for (String nameValue : nameValueArray) {
+                Matcher m = datePatt.matcher(nameValue);
+                if (m.matches()) {
+                    map.put(m.group(1), m.group(2));
+                }
+            }
+            
+            return map;
+        }
+        
+        private void writeOut(java.util.Map<String, String> map, Context context) throws IOException, InterruptedException {
+            StringBuffer buffer = new StringBuffer();
+
+            Iterator iter = map.entrySet().iterator();
+            while (iter.hasNext()) {
+                java.util.Map.Entry entry = (java.util.Map.Entry) iter.next();
+                Object key = entry.getKey();
+                Object val = entry.getValue();
+                if (buffer.length() > 0) {
+                    buffer.append(",");
+                }
+                
+                buffer.append(key + "=" + val);                
+            } 
+            
+            context.write(null, new Text(buffer.toString()));
+        }
+        
+        private int diffTimeInSecond(String endTime, String startTime) {
+            Date d1 = null;
+            Date d2 = null;
+            try {
+                d1 = dateFormat.parse(startTime);
+                d2 = dateFormat.parse(endTime);
+            } catch (ParseException ex) {
+                LOG.error(ex);
+            }    
+            
+            long diff = d2.getTime() - d1.getTime();
+            LOG.info("d2,d1 time=" + d2.getTime() + "," + d1.getTime());
+            int diffMinutes = (int)diff / 1000;   
+            
+            return diffMinutes;
+        }
+        
+        private void doCalc(LinkedList<String> sortedList, Context context) throws IOException, InterruptedException {
+            String first = ""; //sortedList.getFirst();
+            String second = "";
+            java.util.Map<String, String> mapFirst = new HashMap<String, String>(); // field name convert map
+            java.util.Map<String, String> mapSecond = new HashMap<String, String>(); // field name convert map
+            
+            try {
+                while(!sortedList.isEmpty()) {
+                    if (first.isEmpty()) {
+                        first = sortedList.removeFirst();
+                    }
+                    else {
+                        first = second;
+                    }
+                    
+                    second = sortedList.removeFirst();
+                    
+                    // construct map for first and second line
+                    mapFirst = convertToMap(first);
+                    mapSecond = convertToMap(second);
+
+                    // EVENT_TIME_END = second.EVENT_TIME_START
+                    mapFirst.put("event_time_end", mapSecond.get("event_time_start"));
+                    // STATE_END = second.STATE_START
+                    mapFirst.put("state_end", mapSecond.get("state_start"));
+                    // DIF = second.EVENT_TIME_START - first.EVENT_TIME_START
+                    
+                    mapFirst.put("dif", Integer.toString(diffTimeInSecond(mapSecond.get("event_time_start"), mapFirst.get("event_time_start"))));
+                    //write out
+                    writeOut(mapFirst, context);
+                }
+            }
+            catch(NoSuchElementException ex) {
+                LOG.warn("no element left" + ex);
+            }
+        }
                 
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             // 1. remove line with empty eqp_state   -- done in mapper
             // 2. sort all values by event_create_dt
             // 3. pick two consecutive lines and do the calculation
             
-            List<String> sortedList = sortByEvent_create_dt(values);
+            LinkedList<String> sortedList = sortByEvent_create_dt(values);
             
-            for (String value:sortedList) {
-                context.write(key, new Text(value));
-            }
-            //doCalc(sortedList);
+//            for (String value:sortedList) {
+//                context.write(key, new Text(value));
+//            }
+            doCalc(sortedList, context);
             
 //            for (Text value:values) {
 //               context.write(key, value);
