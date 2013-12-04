dd8411b0ffe9063eac10020dfd6a4ffc02d97226
Brad Liu <brad_liu@trend.com.tw>
2013-12-04 19:26:38 +0800

    auto add dead node back to config.

--- hadoop_wordcount/src/memcached_monitor.pl
+++ hadoop_wordcount/src/memcached_monitor.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use POSIX qw(strftime);
+use File::Copy qw(move);
 
 my $arg_nums = $#ARGV + 1;
 print "num args=$arg_nums\n";
@@ -10,54 +11,69 @@ if ($arg_nums < 1) {
     exit;
 }
 
-my $cfg_file = shift(@ARGV);
-my $time_interval = 10; # defaule time is 10 seconds
+my $CFG_FILE = shift(@ARGV);
+my $DEAD_CFG_FILE = "$CFG_FILE.dead";
+my $TIME_INTERVAL = 10; # defaule time is 10 seconds
 if ($arg_nums == 2) {
-    $time_interval = shift(@ARGV);
+    $TIME_INTERVAL = shift(@ARGV);
 }
 
 # init some golbal variable
-our %cfg_properties = ();
-our $memcache_servers = "";
+my @g_memcache_servers = ();
+my @g_dead_memcache_servers = ();
+my $LOG_FILE = "/var/log/memcached_monitor.log";
 
 sub monitor {
-    my @servers = split ',', $memcache_servers;
     my $has_disconnect_server = 0;
     my $connected_server = "";
+    my $disconnected_server = "";
+    my @total_server = (@g_memcache_servers, @g_dead_memcache_servers);
+    my @current_live_server = ();
+    my @current_dead_server = ();
     
-    for my $server (@servers) {
-        $server =~ tr/";//d;
-        my $cmd = "/usr/bin/memcached-tool " . $server . " 2>&1";
+    foreach (@total_server) {
+        $_ =~ tr/";//d;
+        my $cmd = "/usr/bin/memcached-tool " . $_ . " 2>&1";
         print "cmd=$cmd\n";
         my $results = system $cmd;
         my_print("run cmd=$cmd, return result=". $results . "\n");
-        if ($results != 0) {
-            $has_disconnect_server = 1;            
+        
+        if ($results == 0) { # connected
+            #$current_live_server{$_} = 1;
+            push(@current_live_server, $_);
         }
-        else {
-            if ($connected_server eq '') {
-                $connected_server = $connected_server . $server;
-            }
-            else {
-                $connected_server = $connected_server . ", " . $server;
-            }
+        else { # disconnected server
+            #$current_dead_server{$_} = 1;
+            push(@current_dead_server, $_);
         }
     }
     
-    return ($has_disconnect_server, $connected_server);
+    # compare current_live_server with g_memcache_servers
+    if (@g_memcache_servers ~~ @current_live_server) { # the same
+        print "same array\n";
+        $has_disconnect_server = 0;        
+    }
+    else {
+        $has_disconnect_server = 1;
+    }
+
+    $connected_server = join(",", @current_live_server);
+    $disconnected_server = join(",", @current_dead_server);
+    return ($has_disconnect_server, $connected_server, $disconnected_server);
 }
 
 sub save_to_config_file {
     my $has_disconnect_server = shift;
     my $connected_server = shift;
+    my $disconnected_server = shift;
     
     if ($has_disconnect_server == 0) {
         return;
     }
     # if disconnect server happened:
     # save it;
-    open my $in,  '<',  $cfg_file   or die "Can't read old file: $!";
-    open my $out, '>', "$cfg_file.new" or die "Can't write new file: $!";
+    open my $in,  '<',  $CFG_FILE   or die "Can't read old file: $!";
+    open my $out, '>', "$CFG_FILE.new" or die "Can't write new file: $!";
 
     while(<$in>) {
         if ($_ =~ m/^memcache_servers=(.*)/) {
@@ -69,21 +85,66 @@ sub save_to_config_file {
             print $out $_;
         }
     }
+    
+    close($in);
+    close($out);
+    
+    move "$CFG_FILE.new", $CFG_FILE;
+    
+    # update config file for dead server_name
+    open FILE, ">$DEAD_CFG_FILE";
+    print FILE 'memcache_servers="' . $disconnected_server . '";' . "\n";
+    close FILE;
+    # send signal to updater to reload config file
+    `killall -SIGHUP memcached_updator`;    
+}
+
+sub trim {
+    my $string = shift;
+    $string =~ s/^\s+//;
+    $string =~ s/\s+$//;
+    $string =~ tr/";//d;
+    return $string;
+}
+
+sub parse_memcache_server {
+    my $servers = shift;
+    my $init_value = shift;
+    my @live_servers = split ',', $servers;
+    my @servers = ();
+    
+    for my $server (@live_servers) {
+        $server = trim($server);
+        print "add server:$server into hash\n";
+        push(@servers, $server);
+    }
+    
+    return @servers;
 }
 
 sub get_memcached_server {
-    my $my_cfg_file = shift;
-    
-    open(F, $my_cfg_file) or die "failed to open file $!\n";
+    open(F, $CFG_FILE) or die "failed to open file $!\n";
     while (<F>) {
         if ($_ =~ m/^memcache_servers=(.*)/) {
-            $memcache_servers = $1;
-            print "memcached server=$memcache_servers\n";
+            @g_memcache_servers=parse_memcache_server($1, 1);
+            print "debug:@g_memcache_servers\n";
             last;
         }
+    }    
+    close(F);    
+
+    unless (-e $DEAD_CFG_FILE) {
+        return;
     }
     
-    close(F);    
+    open(F, $DEAD_CFG_FILE);
+    while (<F>) {
+        if ($_ =~ m/^memcache_servers=(.*)/) {
+            @g_dead_memcache_servers=parse_memcache_server($1, 0);
+            last;
+        }
+    }    
+    close(F);
 }
 
 sub init {
@@ -103,7 +164,6 @@ sub init {
 # my_print: handmade log function
 ################################################################
 sub my_print {
-    my $LOG_FILE = "/var/log/memcached_monitor.log";
     open(FILE, ">> $LOG_FILE");
     binmode(FILE, ":unix");
     my $time_str = strftime "%F %T", localtime;
@@ -112,7 +172,7 @@ sub my_print {
 }
 
 init();
-get_memcached_server($cfg_file);
-my ($has_disconnect_server, $connected_server) = monitor();
-print "1=$has_disconnect_server, 2=$connected_server\n";
-save_to_config_file($has_disconnect_server, $connected_server);
+get_memcached_server();
+my ($has_disconnect_server, $connected_server, $disconnected_server) = monitor();
+my_print("1=$has_disconnect_server, 2=$connected_server, 3=$disconnected_server\n");
+save_to_config_file($has_disconnect_server, $connected_server, $disconnected_server);
